2af125965e53022b4d64071d056668bbdbc5db89
Tamara <novickaya.toma@mail.ru>
2023-03-18 18:50:47 +0300

    ДЗ-4

---
+++ HW_18_03/HW_18_03_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+// Вариант 1
+// Console.Write("Введите первое число: ");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int numberB = Convert.ToInt32(Console.ReadLine());
+// int res=1;
+
+// for (int i = 0; i < numberB; i++)
+// {
+// res *= numberA;
+// }
+// Console.WriteLine(res);
+
+// Вариант 2
+
---
+++ HW_18_03/HW_18_03_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+// Вариант 1
+
+int[] arrey = new int[8];
+
+for (int i = 0; i < arrey.Length; i++)
+{
+    int x= new Random().Next(0,100);
+    arrey[i]=x;
+   Console.Write($"{arrey[i]}   "); 
+}
+
+// Вариант 2
+int[] arrey = new int[8] {1,2,3,4,5,6,7,8};
+
+for (int i = 0; i < arrey.Length; i++)
+{
+      Console.Write($"{arrey[i]}   "); 
+}
