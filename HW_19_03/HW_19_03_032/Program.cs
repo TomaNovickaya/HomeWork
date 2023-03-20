294b039d151ec5603a29a6788556147525b5ba40
Tamara <novickaya.toma@mail.ru>
2023-03-20 10:29:23 +0300

    ДЗ-5

---
+++ HW_19_03/HW_19_03_032/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] arr=new int [10];
+
+void PrintArrey(int [] arrey)
+{
+    int count = arrey.Length;
+    for (int i = 0; i < count; i++)
+    {
+        int x = new Random().Next(1,100);
+        arrey[i]=x;
+                Console.Write($"   {arrey[i]}");
+
+            }
+    Console.WriteLine();
+   
+}
+
+ PrintArrey(arr);
+int min =arr[0];
+ for (int i = 0; i < arr.Length; i++)
+ {
+    if (arr[i]<min) min=arr[i];     
+       
+ }
+ Console.WriteLine($"Минимальный элемент массива: {min}");
+ 
+ int max =arr[0];
+ for (int i = 0; i < arr.Length; i++)
+ {
+    if (arr[i]>max) max=arr[i];     
+       
+ }
+ Console.WriteLine($"Максимальный элемент массива: {max}");
+
+ int res= max-min;
+ Console.WriteLine($"Разница между максимальным и минимальным: {res}");
