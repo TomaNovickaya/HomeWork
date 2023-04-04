b5701395524e851c98b2c9ddc7d4bfc2f973c6d9
Tamara <novickaya.toma@mail.ru>
2023-04-05 01:02:43 +0300

    ДЗ-5

---
+++ HW_25.03/41/Program.cs
@@ -0,0 +1,33 @@
+﻿// 41. Пользователь вводит с клавиатуры М чисел. Посчитайте, сколько чисел боль 0 ввел пользователь.
+
+int[] nums=new int [5];
+
+void PrintArrey(int [] arrey)
+{
+    int count = arrey.Length;
+    for (int i = 0; i < count; i++)
+    {
+        int x = new Random().Next(-5,6);
+        arrey[i]=x;
+        Console.Write($"   {arrey[i]}");
+    }
+    Console.WriteLine();
+   
+}
+ PrintArrey(nums);
+int count=0;
+
+for (int i = 0; i < nums.Length; i++)
+{
+   
+    if (nums[i]>0)  
+    {
+    count+=1;
+    }
+           
+        
+        
+    
+}
+Console.WriteLine(count);
+
