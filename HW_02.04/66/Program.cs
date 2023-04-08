045ae3844df07ddb42b960c11a8ef03c37946567
Tamara <novickaya.toma@mail.ru>
2023-04-09 01:34:30 +0300

    Промежуточная аттестация

---
+++ HW_02.04/66/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNumber(int n,int m)
+{
+    if(n==0) return 0;
+    else return n + SumNumber(n-1,m);
+    
+    
+}
+Console.WriteLine($"{SumNumber(15,0)}");
